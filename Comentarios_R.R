7c37a3b35906e5a2a78f96d73f8a3d696e018206
VeronicaGoCa <programandoenlinea@gmail.com>
2021-03-25 09:49:18 -0400

    Comentarios en R + proj

---
+++ Comentarios_R.R
@@ -0,0 +1,17 @@
+# COMENTARIOS EN R
+# Ctrl + May(Shift) + c --->Para insertar comentario
+
+# Ctrl + May(shift) + r --->Inserta comentario como sigue
+# Mi comentario -----------------------------------------------------------
+
+
+# Comentarios multilinea --------------------------------------------------
+# Recomendacion desde: https://es.stackoverflow.com/questions/74586/c%C3%B3mo-realizar-comentarios-multi-l%C3%ADnea-en-r
+
+"cometnario 1
+cometnario 2
+cometnario 3
+"
+# Seleccionar todas las lineas necesarias luego aplicar Ctrl + May(Shift) + c
+# comentario 1
+# comentario 2
